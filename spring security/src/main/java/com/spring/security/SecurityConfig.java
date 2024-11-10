ddbeb15e0b963106f7a8362d7c7c1c1a38a74d35
AkshayRJ28 <Akshay.rj2784@gmail.com>
2024-11-11 01:40:19 +0530

    create jdbc user using h2 and added password encoder

--- spring security/src/main/java/com/spring/security/SecurityConfig.java
+++ spring security/src/main/java/com/spring/security/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.spring.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -11,13 +14,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	DataSource datasource;
 	   
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,16 +43,28 @@ public class SecurityConfig {
 	@Bean
 	public UserDetailsService userDetailService() {
 		UserDetails user1 = User.withUsername("user1")
-				.password("{noop}userPass")
+				.password(passwordEncoder().encode("userPass")) //				.password("{noop}userPass") added pass encoder here
 				.roles("USER")
 				.build();
 		
 		UserDetails admin = User.withUsername("admin1")
-				.password("{noop}adminPass")
+				.password(passwordEncoder().encode("adminPass"))
 				.roles("ADMIN")
 				.build();
 		
-		return new InMemoryUserDetailsManager(user1,admin);
+		JdbcUserDetailsManager userDetailsManager = 
+				new JdbcUserDetailsManager(datasource);
+		userDetailsManager.createUser(user1);
+		userDetailsManager.createUser(admin);
+		return userDetailsManager;
+		
+		//return new InMemoryUserDetailsManager(user1,admin);  
+		// disabling inmememory users and used from h2 h2 database
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }

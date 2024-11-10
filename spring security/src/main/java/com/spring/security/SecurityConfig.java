339fb863ee1212c6c6f7902cf1caeb1fd661a2d7
AkshayRJ28 <Akshay.rj2784@gmail.com>
2024-11-10 23:55:10 +0530

    h2 database configure

--- spring security/src/main/java/com/spring/security/SecurityConfig.java
+++ spring security/src/main/java/com/spring/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,9 +21,14 @@ public class SecurityConfig {
 	   
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests((requests) ->
+		requests.requestMatchers("/h2-console/**").permitAll()
+				.anyRequest().authenticated());
 		//http.formLogin(withDefaults());
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.httpBasic(withDefaults());
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+		http.csrf(csrf -> csrf.disable());
 		return http.build();
 	}
 	@Bean
